3e0351d775dc209582595671910c7ad610007cdd
Bruce Gordon <bruce@focusls.com>
2019-02-01 13:16:59 -0500

    1

---
+++ CommerceTraining/Infrastructure/EPiServerCommerceInitializationModule.cs
@@ -0,0 +1,30 @@
+using System.Web.Mvc;
+using System.Web.Routing;
+using EPiServer.Commerce.Routing;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace CommerceTraining.Infrastructure
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
+    public class EPiServerCommerceInitializationModule : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(context.StructureMap()));
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+            CatalogRouteHelper.MapDefaultHierarchialRouter(RouteTable.Routes, false);
+        }
+
+        public void Preload(string[] parameters) { }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
+}
